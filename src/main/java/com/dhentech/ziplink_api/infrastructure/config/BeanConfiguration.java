94156ab9747b76f79932db09697831914f696c83
denilsonDhentech <denilson_contato@outlook.com>
2026-03-06 14:23:23 -0300

    feat: implement UrlController with MockMvc tests and Jackson configuratio

--- src/main/java/com/dhentech/ziplink_api/infrastructure/config/BeanConfiguration.java
+++ src/main/java/com/dhentech/ziplink_api/infrastructure/config/BeanConfiguration.java
@@ -5,6 +5,9 @@ import com.dhentech.ziplink_api.domain.repositories.UrlRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -20,5 +23,14 @@ public class BeanConfiguration {
     @Transactional
     public UrlShortenerService urlShortenerService(UrlRepository urlRepository, ReentrantLock lock) {
         return new UrlShortenerService(urlRepository, lock);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper;
     }
 }
---
+++ src/main/java/com/dhentech/ziplink_api/infrastructure/web/UrlController.java
@@ -0,0 +1,29 @@
+package com.dhentech.ziplink_api.infrastructure.web;
+
+import com.dhentech.ziplink_api.application.dtos.UrlRequest;
+import com.dhentech.ziplink_api.application.dtos.UrlResponse;
+import com.dhentech.ziplink_api.application.services.UrlShortenerService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/urls")
+public class UrlController {
+
+    private final UrlShortenerService urlShortenerService;
+
+    public UrlController(UrlShortenerService urlShortenerService) {
+        this.urlShortenerService = urlShortenerService;
+    }
+
+    @PostMapping("/shorten")
+    public ResponseEntity<UrlResponse> shorten(@RequestBody @Valid UrlRequest request) {
+        UrlResponse response = urlShortenerService.shorten(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+}
