fb6e770f41132b02764180203291433fc8eda42c
denilsonDhentech <denilson_contato@outlook.com>
2026-03-06 11:36:44 -0300

    chore: initial spring boot project structure for ZipLink

---
+++ src/main/java/com/dhentech/ziplink_api/ZipLinkApiApplication.java
@@ -0,0 +1,13 @@
+package com.dhentech.ziplink_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipLinkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipLinkApiApplication.class, args);
+	}
+
+}
